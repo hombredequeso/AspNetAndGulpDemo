90195cb017cee5becf02e79587fa5e4162f6cad3
Mark Cheeseman <mcheesemanau@gmail.com>
2015-04-15 23:14:45 +1000

    Add Karma
    Add karma infrastructure and sample test.

---
+++ Webapp/MyDomain/tests/MyPageViewModelSpec.js
@@ -0,0 +1,11 @@
+﻿describe('MyPageViewModel Tests', function () {
+    it("ViewModel exists", function () {
+        var viewModel = myApp.myDomain.myPageViewModel();
+        expect(viewModel).toBeDefined();
+    });
+
+    it("description is correctly defined", function () {
+        var viewModel = myApp.myDomain.myPageViewModel();
+        expect(viewModel.description).toBe("DescriptionSetByViewModelY");
+    });
+});
